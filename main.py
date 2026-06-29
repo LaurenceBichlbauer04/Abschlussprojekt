b4a96939f0751afa346a8dcb706d69df65eaa166
Laurence Bichlbauer <lth.bichlbauer04@mci4me.at>
2026-06-29 15:42:39 +0200

    Taskbar commit

--- main.py
+++ main.py
@@ -2,17 +2,37 @@ import streamlit as st
 from src.person import Person
 from src.ekg_data import EKGdata
 from src.read_data import load_person_data, get_name_to_id
+from pages import overview, sleep_analysis, account, ekg_analysis
 
 st.set_page_config(
+    layout="wide",
     page_title="Schlafanalyse",
-    page_icon="🌙",
-    layout="wide"
+    page_icon= "🌙"
+    )
+
+st.sidebar.title("🌙 Schlafanalyse")
+
+page = st.sidebar.radio(
+    "",
+    [
+        "🏠 Übersicht",
+        "💤 Schlafanalyse",
+        "❤️ EKG-Datenanalyse",
+        "👤 Benutzer"
+    ],
+    label_visibility="collapsed",
+    horizontal= False
 )
-#set Pages
-overview = st.Page("pages/overview.py", title="Übersicht", icon="🏠")
-analysis = st.Page("pages/analysis.py", title="Schlafanalyse", icon="📊")
-account = st.Page("pages/account.py", title="Schlafphasen", icon="📊")
 
-pg = st.navigation([overview, analysis, account])
+if page == "🏠 Übersicht":
+    overview.show()
 
+elif page == "📊 Analyse":
+    sleep_analysis.show()
+
+elif page == "❤️ EKG-Datenanalyse":
+    ekg_analysis.show()
+
+elif page == "👤 Account":
+    account.show()
 
---
+++ pages/account.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from src.person import Person
+from src.ekg_data import EKGdata
+from src.read_data import load_person_data, get_name_to_id
+
+def show():
+
+
+
+    st.title("EKG Analyse")
+
+    st.divider()
+
+    person_data = load_person_data()
+    name_to_id = get_name_to_id(person_data)
+
+
+
+    selected_name = st.selectbox("Person auswählen", list(name_to_id.keys()))
+
+    selected_id = name_to_id[selected_name]
+
+    st.divider()
+
+    person = Person.load_by_id(selected_id)
+    ekg_data = EKGdata.load_by_id(selected_id)
+
+
+
+    left_col, right_col = st.columns([1, 2])
+
+
+    with left_col:
+        st.subheader(person.get_full_name())
+        st.write("**Alter:**", person.calc_age())
+        st.write("**Maximale Herzfrequenz:**", person.calc_max_heart_rate())
+        st.write("**Geschlecht:**", person.gender)
+    
+    with right_col:
+        st.image(person.get_image(), width=250)
