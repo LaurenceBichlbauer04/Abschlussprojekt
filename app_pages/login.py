aa61374211e223e13771214cbd6b1340ac127a80
laurencebichlbauer04 <lth.bichlbauer04@mci4me.at>
2026-07-04 20:45:58 +0200

    registration Page

--- app_pages/login.py
+++ app_pages/login.py
@@ -3,10 +3,19 @@ import streamlit as st
 from app_pages import registration
 
 def show():
+
+    if "page" not in st.session_state:
+        st.session_state.page = "login"
+
+    if st.session_state.page == "register":
+        registration.show()
+        st.stop()
+
+
     st.title("Login")
 
-    username = st.text_input("Benutzername")
-    password = st.text_input("Passwort", type="password")
+    username = st.text_input("Benutzername", key="login_username")
+    password = st.text_input("Passwort", type="password", key="login_password")
 
     cols = st.columns(2)
     with cols[0]:
@@ -20,5 +29,5 @@ def show():
                 st.error("Benutzername oder Passwort falsch")
     with cols[1]:
         if st.button("Registrieren", use_container_width=True):
-            registration.show()
-            st.stop()
+            st.session_state.page = "register"
+            st.rerun()
